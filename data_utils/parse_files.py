b58a211250e42734285c97c305436dd662cf9f84
John Plevyak <jplevyak@gmail.com>
2017-10-29 17:18:35 -0700

    Example of simple round trip.

--- data_utils/parse_files.py
+++ data_utils/parse_files.py
@@ -336,8 +336,12 @@ def save_generated_example(filename, generated_sequence, useTimeDomain=False, sa
 def audio_unit_test(filename, filename2):
     data, bitrate = read_wav_as_np(filename)
     time_blocks = convert_np_audio_to_sample_blocks(data, 1024)
-    ft_blocks = time_blocks_to_fft_blocks(time_blocks)
+    print('time_blocks', len(time_blocks), len(time_blocks[0]))
+    ft_blocks = time_blocks_to_fft_blocks(time_blocks, count=1)
+    print('ft_blocks', len(ft_blocks), len(ft_blocks[0]))
     time_blocks = fft_blocks_to_time_blocks(ft_blocks)
+    print('time_blocks', len(time_blocks), len(time_blocks[0]))
     song = convert_sample_blocks_to_np_audio(time_blocks)
+    print(song.shape)
     write_np_as_wav(song, bitrate, filename2)
     return
---
+++ unit_test.py
@@ -0,0 +1,6 @@
+from data_utils.parse_files import *
+import config.nn_config as nn_config
+
+audio_unit_test("datasets/YourMusicLibrary/wave/Happy.wav",
+		"datasets/YourMusicLibrary/wave/Happy2.wav")
+
